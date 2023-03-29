b8335a6ce278bac775ff502fa39dbf37622a24ef
AndreySlabov <andrey.slabov@devexpress.com>
2023-03-29 12:22:17 +0400

    vb

--- VB/View/MainView.xaml.vb
+++ VB/View/MainView.xaml.vb
@@ -1,24 +1,11 @@
-﻿' Developer Express Code Central Example:
-' How to: Use the ViewModelBase Class to Inherit View Models
-' 
-' This example demonstrates how to use the ViewModelBase class for inheriting view
-' models.
-' For additional information, refer to the following topic:
-' Getting
-' Started with DevExpress MVVM Framework. Commands and ViewModels.
-' (https://community.devexpress.com/blogs/wpf/archive/2013/08/29/getting-started-with-devexpress-mvvm-framework-commands-and-view-models.aspx)
-' 
-' You can find sample updates and versions for different programming languages here:
-' http://www.devexpress.com/example=E5169
-
-Imports System.Windows.Controls
+﻿Imports System.Windows.Controls
 
 Namespace Example.View
-    Partial Public Class MainView
-        Inherits UserControl
+	Partial Public Class MainView
+		Inherits UserControl
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
-    End Class
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
 End Namespace
